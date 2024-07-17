260f3b3c381a3a0602590152e6b0ec8edf3e8d7b
Arghya Ghoshal <159574844+arghyacodes@users.noreply.github.com>
2024-07-17 17:29:15 +0000

    Conversion of data types

---
+++ 01_basics/03_ConversionOperations.js
@@ -0,0 +1,24 @@
+/*
+let age = 23
+
+console.log(age);
+console.log(typeof age);
+
+let input = "23a"   //Not A Number NaN if string contains something other than numbers
+
+console.log(input);
+console.log(typeof input);
+let convertedInput = Number(input)
+console.log(convertedInput);
+console.log(typeof convertedInput);
+
+*/
+// "23" -> 23
+// "23a" -> NaN
+// 1 -> true 0-> false
+
+let isUser = 1
+console.log(typeof isUser);
+let isUserBool = Boolean(isUser)
+console.log(typeof isUserBool);
+console.log(isUserBool);
