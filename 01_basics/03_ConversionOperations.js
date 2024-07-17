01cb65a3f15438cd0c93a809a19811bb6176174d
Arghya Ghoshal <159574844+arghyacodes@users.noreply.github.com>
2024-07-17 17:56:22 +0000

    operators

--- 01_basics/03_ConversionOperations.js
+++ 01_basics/03_ConversionOperations.js
@@ -17,8 +17,23 @@ console.log(typeof convertedInput);
 // "23a" -> NaN
 // 1 -> true 0-> false
 
+/*
 let isUser = 1
 console.log(typeof isUser);
 let isUserBool = Boolean(isUser)
 console.log(typeof isUserBool);
 console.log(isUserBool);
+*/
+
+//*****************************OPERATIONS***************************************/
+
+// let str1 = "Hello "
+// let str2 = "World"
+// console.log(str1);
+// str1 += str2
+// console.log(str1);
+
+let count = 10;
+console.log(count++)//count++
+console.log(count)
+console.log(++count)//++count
