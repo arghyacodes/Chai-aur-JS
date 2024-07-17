c64196b0fe10b1f74b96f5193f0a03e9f9ab018e
Arghya Ghoshal <159574844+arghyacodes@users.noreply.github.com>
2024-07-17 16:28:03 +0000

    Data Types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; //all codes will be treated as new js version
+// alert(3+3) //we are using nodeJs not browser so it will not act similar to browser
+let name = "Arghya"//string
+let age = 22//int
+let isLoggedIn = true//boolean
+let state;//null
+
+//other data types
+/*
+bigint,
+null -> standalone value,
+undefined,
+symbol ->unique,
+
+object
+*/
+
+console.log(typeof null) //object
+console.log(typeof state) //undefined
